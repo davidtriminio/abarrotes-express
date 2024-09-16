f7acc5130bb81e772ec0a303b2c316bb1a5f959c
David Triminio <marlon.gonzales@unah.hn>
2024-09-15 21:49:05 -0600

    ⚡Optimiza tabla de últimas ordenes
    -Limita las filas mostradas, muestra solo 5 en lugar de todas las
    ordenes nuevas/
    - Elimina la paginación, ya que solo necesita mostrar las últimas 5
      ordenes.
      - Mejora la interfaz de los métodos de pago en la tabla de últimas
        ordenes.

--- app/Filament/Resources/OrdenResource/Widgets/UltimasOrdenes.php
+++ app/Filament/Resources/OrdenResource/Widgets/UltimasOrdenes.php
@@ -8,16 +8,17 @@ use Filament\Tables\Columns\SelectColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Contracts\Pagination\CursorPaginator;
+use Illuminate\Database\Eloquent\Builder;
 
 class UltimasOrdenes extends BaseWidget
 {
     protected int | string | array $columnSpan = 'full';
+    protected static bool $isLazy = false;
     public function table(Table $table): Table
     {
         return $table
-            ->query(OrdenResource::getEloquentQuery())
-            ->paginated(5)
-            ->paginationPageOptions([1,5,10])
+            ->query(OrdenResource::getEloquentQuery()->limit(5)->where('estado_entrega', 'nuevo'))
             ->defaultSort('created_at', 'desc')
             ->columns([
                 TextColumn::make('user.name')
@@ -25,7 +26,23 @@ class UltimasOrdenes extends BaseWidget
 
                 TextColumn::make('total_final'),
 
-                TextColumn::make('metodo_pago'),
+                TextColumn::make('metodo_pago')
+                    ->badge()
+                    ->icon(fn(string $state): string => match ($state) {
+                        'par' => 'heroicon-o-banknotes',
+                        'efectivo' => 'heroicon-o-currency-dollar',
+                        'tarjeta' => 'heroicon-o-credit-card',
+                    })
+                    ->color(fn(string $state): string => match ($state) {
+                        'par' => 'primary',
+                        'efectivo' => 'success',
+                        'tarjeta' => 'warning',
+                    })
+                    ->formatStateUsing((fn(string $state): string => match ($state) {
+                        'par' => 'Pago al recibir',
+                        'efectivo' => 'Efectivo',
+                        'tarjeta' => 'Tarjeta',
+                    })),
 
                 SelectColumn::make('estado_pago')
                     ->options([
@@ -41,6 +58,7 @@ class UltimasOrdenes extends BaseWidget
                         'entregado' => 'Entregado',
                         'cancelado' => 'Cancelado',
                     ])
-            ]);
+            ])
+            ->paginated(false);
     }
 }

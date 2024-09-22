211a375f0d74519fc48ec7b5ab16ae2709c2523f
selvinPlata98 <selvin.plata@unah.hn>
2024-09-22 08:48:47 -0600

    Implementación de modelo de proveedor

---
+++ app/Models/Proveedor.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Proveedor extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'nombre',
+        'id_productos',
+        'contracto',
+        'cantidad_producto',
+        'pago',
+        'estado',
+    ];
+
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class);
+    }
+    
+}

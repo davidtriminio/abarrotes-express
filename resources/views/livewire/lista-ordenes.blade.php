bde2c2a8dabce3fc726281c6e1584fc2194dcdc6
Selvin Plata <selvin.plata@unah.hn>
2024-10-02 07:31:05 -0600

    🐧 modificación de la tabla de ordenes

--- resources/views/livewire/lista-ordenes.blade.php
+++ resources/views/livewire/lista-ordenes.blade.php
@@ -32,115 +32,63 @@
                             </thead>
 
                             <tbody class="[&_tr:last-child]:border-0">
+                            @foreach($ordenes as $orden)
+                            @php
+                                $estado_orden = '';
+                                 if($orden->estado_entrega == 'nuevo'){
+                                     $estado_orden = '<span class="mr-1 icon-[ph--sparkle]">Nuevo</span>';
+                                 } elseif ($orden->estado_entrega == 'procesado'){
+                                     $estado_orden = '<span class="mr-1 icon-[uim--process]">En Proceso</span>';
+                                 } elseif ($orden->estado_entrega == 'enviado'){
+                                     $estado_orden = '<span class="mr-1 icon-[bi--truck]">Enviado</span>';
+                                 } elseif ($orden->estado_entrega == 'entregado'){
+                                     $estado_orden = '<span class="mr-1 icon-[carbon--box]">Entregado</span>';
+                                 } else{
+                                     $estado_orden = '<span class=".bg-cancelado">Cancelado </span>';
+                                 }
 
-                            <tr class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">ORD001</td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">232.06</td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-                                <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-tarjeta">
-                                    <span class="mr-1 icon-[ion--card-outline]"></span>
-                                    Tarjeta
-                                </span>
-                                </td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-                                <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-procesando">
-                                    <span class="mr-1 icon-[uim--process]"></span>
-                                    Procesando
-                                </span>
-                                </td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-                                <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-nuevo">
-                                    <span class="mr-1 icon-[ph--sparkle]"></span>
-                                    Nuevo
-                                </span>
-                                </td>
+                                
+                                $metodos_pago = '';
+                                 if($orden->metodo_pago == 'efectivo'){
+                                     $metodos_pago = '<span class="mr-1 icon-[ion--card-outline]">efectivo</span>';
+                                 } elseif ($orden->metodo_pago == 'tarjeta'){
+                                     $metodos_pago = '<span class="mr-1 icon-[teenyicons--money-stack-outline]">Tarjeta de crédito o débito</span>';
+                                 } elseif ($orden->metodo_pago == 'par'){
+                                     $metodos_pago = '<span class="mr-1 icon-[teenyicons--shop-outline]">Pago al Recibir</span>';
+                                 } 
+
+                                 $estado_pago = '';
+                                 if($orden->estado_pago == 'fallo'){
+                                     $estado_pago = '<span class="mr-1 icon-[teenyicons--exclamation-circle-outline]">Falló</span>';
+                                 } elseif ($orden->estado_pago == 'pendiente'){
+                                     $estado_pago = '<span class="bg-orange-700 py-1 px-3 rounded text-white shadow">Pendiente</span>';
+                                 } else {
+                                     $estado_pago = '<span class="mr-1 icon-[uim--process]">Pagado</span>';
+                                 }
+
+                                 $moneda = '';
+                                 if ($orden -> moneda == 'usd'){
+                                     $moneda = 'usd';
+                                  } elseif ($orden -> moneda == 'lps'){
+                                      $moneda = 'lps';
+                                  } else{
+                                       $moneda = 'eur';
+                                  }
+                            @endphp
+                            <tr wire:key="{{$orden -> id}}"
+                                class="odd:bg-white even:bg-gray-100 dark:odd:bg-slate-900 dark:even:bg-slate-800">
+                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">{{$orden->id}}</td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{{ $orden->total_final }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{!! $metodos_pago !!}</td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{!! $estado_pago !!}</td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{!! $estado_orden !!}</td>
                                 <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0 text-right">
-                                    <button class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
-                                        Ver Orden
-                                    </button>
+                                    <a href="/mis-pedidos/{{$orden->id}}"
+                                       class="bg-slate-600 text-white py-2 px-4 rounded-md hover:bg-slate-500">Ver Orden</a>
                                 </td>
                             </tr>
-                            <tr class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">ORD002</td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">5.00</td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-                                <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-efectivo">
-                                    <span class="mr-1 icon-[teenyicons--money-stack-outline]"></span>
-                                    Efectivo
-                                </span>
-                                </td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-                                <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-error">
-                                    <span class="mr-1 icon-[teenyicons--exclamation-circle-outline]"></span>
-                                    Error
-                                </span>
-                                </td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-                                <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-en-proceso">
-                                    <span class="mr-1 icon-[uim--process]"></span>
-                                    En Proceso
-                                </span>
-                                </td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0 text-right">
-                                    <button class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
-                                        Ver Orden
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">ORD003</td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">100.00</td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-                                <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-pago-al-recibir">
-                                    <span class="mr-1 icon-[teenyicons--shop-outline]"></span>
-                                    Pago al Recibir
-                                </span>
-                                </td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-                                <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-pagado">
-                                    <span class="mr-1 icon-[material-symbols--check-circle-outline]"></span>
-                                    Pagado
-                                </span>
-                                </td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-                                <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-enviado">
-                                    <span class="mr-1 icon-[bi--truck]"></span>
-                                    Enviado
-                                </span>
-                                </td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0 text-right">
-                                    <button class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
-                                        Ver Orden
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">ORD004</td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">57.89</td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-                                <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-efectivo">
-                                    <span class="mr-1 icon-[teenyicons--money-stack-outline]"></span>
-                                    Efectivo
-                                </span>
-                                </td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-                                <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-enviado">
-                                    <span class="mr-1 icon-[material-symbols--check-circle-outline]"></span>
-                                    Pagado
-                                </span>
-                                </td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-                                <span class="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-entregado">
-                                    <span class="mr-1 icon-[carbon--box]"></span>
-                                    Entregado
-                                </span>
-                                </td>
-                                <td class="p-4 align-middle [&:has([role=checkbox])]:pr-0 text-right">
-                                    <button class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
-                                        Ver Orden
-                                    </button>
-                                </td>
-                            </tr>
+                        @endforeach
+                            
                             </tbody>
                         </table>
                     </div>

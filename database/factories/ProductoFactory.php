09b023ae67d068cf25da2c0afa74c14f612af52a
David Triminio <marlon.gonzales@unah.hn>
2024-10-10 11:41:24 -0600

    ✨Agrega migración deproductos
    - Agrega migración para tener productos con sus respectivas categorías y
      marcas

---
+++ database/factories/ProductoFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Categoria;
+use App\Models\Marca;
+use App\Models\Producto;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class ProductoFactory extends Factory
+{
+    protected $model = Producto::class;
+
+    public function definition(): array
+    {
+        return [
+            'nombre' => $this->faker->unique()->word(),
+            'imagenes' => null,
+            'imagen1' => null,
+            'imagen2' => null,
+            'imagen3' => null,
+            'imagen4' => null,
+            'imagen5' => null,
+            'descripcion' => $this->faker->paragraph(),
+            'precio' => $this->faker->numberBetween(1,500),
+            'disponible' => true,
+            'cantidad_disponible' => $this->faker->numberBetween(10, 500),
+            'en_oferta' => $this->faker->boolean(),
+            'porcentaje_oferta' => $this->faker->numberBetween(1,100),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'marca_id' => Marca::inRandomOrder()->first(),
+            'categoria_id' => Categoria::inRandomOrder()->first(),
+        ];
+    }
+}

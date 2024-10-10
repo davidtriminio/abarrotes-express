358bf9df0e5b24998595169c00db4a479f74d09d
David Triminio <marlon.gonzales@unah.hn>
2024-10-10 11:44:12 -0600

    🧩Actualiza migración de Ordenes
    - Actualiza los dtos generados por orden, de manera que se generen datos
      similares a los necesarios.

--- database/factories/OrdenFactory.php
+++ database/factories/OrdenFactory.php
@@ -13,17 +13,26 @@ class OrdenFactory extends Factory
 
     public function definition(): array
     {
+        if (Orden::where('estado_entrega', 'entregado')) {
+            $fecha_entrega = $this->faker->dateTimeBetween('-90 days', '-1 days');
+            $estado_pago = 'pagado';
+        }
+        else {
+            $fecha_entrega = null;
+            $estado_pago = $this->faker->randomElement(['pagado', 'procesando']);
+        }
         return [
-            'created_at' => Carbon::now(),
+            'created_at' => $this->faker->dateTimeBetween('-1 year', '-1 day'),
             'updated_at' => Carbon::now(),
-            'sub_total' => $this->faker->randomFloat(),
-            'total_final' => $this->faker->randomFloat(),
-            'metodo_pago' => $this->faker->word(),
-            'estado_pago' => $this->faker->word(),
-            'estado_entrega' => $this->faker->word(),
-            'costos_envio' => $this->faker->word(),
-
-            'user_id' => User::factory(),
+            'user_id' => User::inRandomOrder()->first(),
+            'sub_total' => $this->faker->numberBetween(10, 100),
+            'total_final' => $this->faker->numberBetween(0, 10000),
+            'metodo_pago' => $this->faker->randomElement(['par', 'efectivo', 'tarjeta']),
+            'estado_pago' => $estado_pago,
+            'estado_entrega' => $this->faker->randomElement(['nuevo', 'procesado', 'enviado', 'entregado', 'cancelado']),
+            'costos_envio' => $this->faker->numberBetween(90, 95),
+            'fecha_entrega' => $fecha_entrega,
+            'notas' => $this->faker->text()
         ];
     }
 }

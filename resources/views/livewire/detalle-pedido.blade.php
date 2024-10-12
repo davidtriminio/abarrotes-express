5a827357467f58049ec5b70f798dfe9a0ae495d7
David Triminio <marlon.gonzales@unah.hn>
2024-10-11 19:00:22 -0600

    ⚙️ Configura nueva función para municipios y deptos
    - Configura función para cargar en variable de tipo arreglo las opciones para cada
      municipio y cada departamento.

--- resources/views/livewire/detalle-pedido.blade.php
+++ resources/views/livewire/detalle-pedido.blade.php
@@ -1,3 +1,21 @@
+<?php
+$departamentos = json_decode(file_get_contents(resource_path('data/departamentos.json')), true);
+$municipios = json_decode(file_get_contents(resource_path('data/municipios.json')), true);
+
+/*Inicializar las opciones y almacenar los departamentos y municipios en opciones y arreglos*/
+$departamentoOpciones = '';
+foreach ($departamentos['departamentos'] as $key => $value) {
+    $departamentoOpciones .= "<option value=\"$key\">$value</option>";
+}
+
+$municipioOpciones = '<option value="">Seleccione un municipio</option>';
+foreach ($municipios['municipios'] as $departamento => $mun) {
+    foreach ($mun as $key => $value) {
+        $municipioOpciones .= "<option class=\"municipio-option $departamento\" data-departamento=\"$departamento\" value=\"$key\">$value</option>";
+    }
+}
+dd($departamentoOpciones . $municipioOpciones);
+?>
 <div>
 
 </div>

6895fba5d4a9e75d77a04aad0df511f63c412216
Selvin Plata <selvin.plata@unah.hn>
2024-10-17 11:53:22 -0600

    🐧 Creacion de la migración para las promociones

---
+++ database/migrations/2024_10_17_114416_create_promociones_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promociones', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('producto_id')->nullable()->constrained('producto')->onDelete('restrict');
+            $table->boolean('estado');
+            $table->dateTime('fecha_inicio');
+            $table->dateTime('fecha_expiracion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promociones');
+    }
+};

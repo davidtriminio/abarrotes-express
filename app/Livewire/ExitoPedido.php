81e40b5136de17980a8d249a8c71821cc38814ef
David Triminio <marlon.gonzales@unah.hn>
2024-10-18 20:18:17 -0600

    ✨Agrega configuración para vista de exito
    - Agrega condicional para definir si hay o no ordenes
    - Agrega estilos para errores en página de pedidos exitosos, sino hay
      ninguno.
    - Agrega funcionalidad para poder pasar datos de la última orden
      exitosa.

--- app/Livewire/ExitoPedido.php
+++ app/Livewire/ExitoPedido.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Orden;
 use Livewire\Component;
 
 class ExitoPedido extends Component
 {
     public function render()
     {
-        return view('livewire.exito-pedido');
+        $ultima_orden = Orden::with('direccion')->where('user_id', auth()->id())->latest()->first();
+        if ($ultima_orden) {
+            return view('livewire.exito-pedido', [
+                'orden' => $ultima_orden
+            ]);
+        } else {
+           return view('livewire.exito-pedido', [ 'orden' => null]);
+        }
     }
 }
--- resources/views/livewire/exito-pedido.blade.php
+++ resources/views/livewire/exito-pedido.blade.php
@@ -1,20 +1,30 @@
-<div class="bg-green-200">
-    <div class="my-5 bg-green-200 py-4">
-        <div class="max-w-4xl mx-auto">
-            <main class="flex flex-col md:flex-row gap-4">
-                <div class="flex-grow bg-green-500 text-white p-8 rounded-lg shadow">
-                    <div class="flex justify-center mb-6">
-                        <span class="icon-[material-symbols--check-circle] h-20 w-20"></span>
+@if($orden)
+    <div class="bg-green-200">
+        <div class="my-5 bg-green-200 py-4">
+            <div class="max-w-4xl mx-auto">
+                <main class="flex flex-col md:flex-row gap-4">
+                    <div class="flex-grow bg-green-500 text-white p-8 rounded-lg shadow">
+                        <div class="flex justify-center mb-6">
+                            <span class="icon-[material-symbols--check-circle] h-20 w-20"></span>
+                        </div>
+                        <h1 class="text-3xl font-bold text-center mb-2">GRACIAS</h1>
+                        <h2 class="text-2xl font-semibold text-center mb-4">SU ORDEN HA SIDO CONFIRMADA</h2>
+                        <p class="text-center mb-8">
+                            Se enviará un correo de confirmación a <span
+                                class="font-black text-blue-800">{{auth()->user()->email}}</span> dentro de poco.
+                        </p>
                     </div>
-                    <h1 class="text-3xl font-bold text-center mb-2">GRACIAS</h1>
-                    <h2 class="text-2xl font-semibold text-center mb-4">SU ORDEN HA SIDO CONFIRMADA</h2>
-                    <p class="text-center mb-8">
-                        Se enviará un correo de confirmación a <span
-                            class="font-black text-blue-800">{{auth()->user()->email}}</span> dentro de poco.
-                    </p>
-                </div>
-            </main>
+                </main>
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+@else
+    <div class="bg-red-500 my-5 py-4">
+        <div class="my-5 py-4">
+            <div class="max-w-4xl mx-auto">
+                <h1 class="text-3xl font-bold text-center mb-2">No se encontró ningún pedido.</h1>
+            </div>
         </div>
     </div>
-    <!-- /.container -->
-</div>
+@endif

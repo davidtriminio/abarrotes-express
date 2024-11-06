782803f0129e22b1083ddd2ad67a718b17ed3b82
LAOV123 <laortez@unah.hn>
2024-11-06 14:36:08 -0600

    Se cambiaron iconos para utilizar libreria yesicon
    -Iconos de la navbar
    
    -Icono de favoritos

--- resources/views/livewire/complementos/navbar.blade.php
+++ resources/views/livewire/complementos/navbar.blade.php
@@ -122,45 +122,22 @@
             </span>
                                         <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
                                            href="{{ route('ordenes') }}">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
-                                            </svg>
-
+                                            <span class="icon-[icon-park-outline--order] text-xl"></span>
                                             Ordenes
                                         </a>
                                         <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
                                            href="{{ route('perfil') }}">
-                                            <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg"
-                                                 width="24"
-                                                 height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/>
-                                                <circle cx="9" cy="7" r="4"/>
-                                                <path d="M22 21v-2a4 4 0 0 0-3-3.87"/>
-                                                <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
-                                            </svg>
+                                            <span class="icon-[iconamoon--profile-bold] text-xl"></span>
                                             Perfil
                                         </a>
                                         <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
                                            href="{{ route('quejasugerencia') }}">
-                                            <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg"
-                                                 width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                <!-- Icono de comentario para Quejas y Sugerencias -->
-                                                <path d="M21 15a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h4l5 5v-5h7z"/>
-                                            </svg>
+                                            <span class="icon-[bx--comment] text-xl"></span>
                                             Quejas y sugerencias
                                         </a>
                                         <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
                                            href="{{ route('reporteproblema') }}">
-                                            <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg"
-                                                 width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                <!-- Icono de advertencia para Reportar un problema -->
-                                                <path d="M12 2L2 22h20L12 2z"/>
-                                                <line x1="12" y1="16" x2="12" y2="12"/>
-                                                <line x1="12" y1="20" x2="12" y2="20"/>
-                                            </svg>
+                                            <span class="icon-[mynaui--danger-triangle] text-xl"></span>
                                             Reportar un problema
                                         </a>
 
@@ -173,12 +150,7 @@
 
                                             <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
                                                href="/admin">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                     viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                     class="size-6">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                          d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"/>
-                                                </svg>
+                                                <span class="icon-[tabler--star] text-xl"></span>
                                                 Ir al Panel Administrativo
                                             </a>
                                             @endif
@@ -188,14 +160,7 @@
       </span>
                                             <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500"
                                                href="{{ route('logout') }}">
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                                     fill="current"
-                                                     class="size-6">
-                                                    <path fill-rule="evenodd"
-                                                          d="M9.53 2.47a.75.75 0 0 1 0 1.06L4.81 8.25H15a6.75 6.75 0 0 1 0 13.5h-3a.75.75 0 0 1 0-1.5h3a5.25 5.25 0 1 0 0-10.5H4.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0Z"
-                                                          clip-rule="evenodd"/>
-                                                </svg>
-
+                                                <span class="icon-[lets-icons--back] text-xl" ></span>
                                                 Cerrar Sesión
                                             </a>
                                         </div>
--- resources/views/livewire/producto.blade.php
+++ resources/views/livewire/producto.blade.php
@@ -94,16 +94,12 @@
                                         @if($esFavorito)
                                             <!-- Corazón lleno para favorito -->
                                             <button wire:click="eliminarFavorito" class="text-red-500 hover:text-red-700 focus:outline-none">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 0 24 24" fill="currentColor">
-                                                    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
-                                                </svg>
+                                                <span class="icon-[fxemoji--redheart] text-3xl"></span>
                                             </button>
                                         @else
                                             <!-- Corazón sin relleno para cuando no es favorito -->
                                             <button wire:click="agregarFavorito" class="text-gray-500 hover:text-gray-700 focus:outline-none">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                                                    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
-                                                </svg>
+                                                <span class="icon-[mdi--heart-outline] text-3xl"></span>
                                             </button>
                                         @endif
                                     @endauth

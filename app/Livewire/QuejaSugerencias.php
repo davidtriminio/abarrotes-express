34cfbec11044fc8c4c0ae4946512c9f3ecf539c5
LAOV123 <laortez@unah.hn>
2024-12-04 19:58:45 -0600

    Se corrigio validaciones de quejas-sugerencias y reporte-problemas

--- app/Livewire/QuejaSugerencias.php
+++ app/Livewire/QuejaSugerencias.php
@@ -14,13 +14,28 @@ class QuejaSugerencias extends Component
     public $tipo;
     public $descripcion;
 
+    protected $rules = [
+        'titulo' => 'required|string|max:30|regex:/^[a-zA-Z0-9\sáéíóúÁÉÍÓÚñÑ]+$/',
+        'tipo' => 'required|string|in:Queja,Sugerencia',
+        'descripcion' => 'required|string|max:500|regex:/^[a-zA-Z0-9\sáéíóúÁÉÍÓÚñÑ.,!?]+$/',
+    ];
+
+    protected $messages = [
+        'titulo.required' => 'El título es obligatorio.',
+        'titulo.string' => 'El título debe ser una cadena de texto.',
+        'titulo.max' => 'El título no puede superar los 30 caracteres.',
+        'titulo.regex' => 'El titulo solo debe contener letras.',
+        'tipo.required' => 'Debe seleccionar un tipo de registro.',
+        'tipo.in' => 'El tipo debe ser una "Queja" o una "Sugerencia".',
+        'descripcion.required' => 'La descripción es obligatoria.',
+        'descripcion.string' => 'La descripción debe ser una cadena de texto.',
+        'descripcion.max' => 'La descripción no puede superar los 500 caracteres.',
+        'descripcion.regex' => 'La descripcion solo debe contener letras y numeros',
+    ];
+
     public function crearQuejaSugerencia()
     {
-        $this->validate([
-            'titulo' => 'required|string|max:30',
-            'tipo' => 'required|string|in:Queja,Sugerencia',
-            'descripcion' => 'required|string|max:500',
-        ]);
+        $this->validate();
 
         QuejaSugerencia::create([
             'titulo' => $this->titulo,
@@ -30,10 +45,7 @@ class QuejaSugerencias extends Component
 
         $this->alert('success', 'Queja o sugerencia creada con éxito.');
 
-
-        $this->titulo = '';
-        $this->tipo = '';
-        $this->descripcion = '';
+        $this->reset(['titulo', 'tipo', 'descripcion']);
     }
 
     public function render()
--- resources/views/livewire/reporte-problema.blade.php
+++ resources/views/livewire/reporte-problema.blade.php
@@ -1,5 +1,4 @@
 <div class="qsrp-container mx-auto mt-5">
-    <!-- Título centrado -->
     <div class="text-center mb-12 lg:mb-20 mt-12">
         <h2 class="text-5xl font-bold mb-4"><span class="text-primary">Reporte </span><span class="text-primary">de Problemas</span></h2>
         <p class="my-7">Informa cualquier inconveniente para que podamos ayudarte y mejorar tu experiencia.</p>
@@ -10,7 +9,7 @@
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div class="qsrp-form-group">
                     <label class="qsrp-form-label">Título</label>
-                    <input type="text" wire:model="titulo" class="qsrp-form-input" maxlength="30" required oninput="updateTitleCharacterCount(this)">
+                    <input type="text" wire:model="titulo" class="qsrp-form-input" oninput="updateTitleCharacterCount(this)">
                     <div class="text-right">
                         <span id="titleCharCount" class="text-gray-600">
                             <span id="titleRemainingCount">30</span>/30 caracteres restantes
@@ -20,7 +19,7 @@
                 </div>
                 <div class="qsrp-form-group">
                     <label class="qsrp-form-label">Sección</label>
-                    <select wire:model="seccion" class="qsrp-form-select" required>
+                    <select wire:model="seccion" class="qsrp-form-select">
                         <option value="">Seleccione una sección</option>
                         <option value="Inicio">Inicio</option>
                         <option value="Productos">Productos</option>
@@ -29,7 +28,6 @@
                         <option value="Cupones">Cupones</option>
                         <option value="Carrito">Carrito</option>
                         <option value="Perfil">Perfil</option>
-                        <!-- Verificar si el usuario está autenticado y si tiene el rol adecuado -->
                         @if(auth()->check() && (auth()->user()->hasRole('Administrador') || auth()->user()->hasRole('Superadmin')))
                             <option value="Panel Administrativo">Panel Administrativo</option>
                         @endif
@@ -39,7 +37,7 @@
             </div>
             <div class="qsrp-form-group">
                 <label class="qsrp-form-label">Descripción</label>
-                <textarea wire:model="descripcion" class="qsrp-form-textarea" maxlength="500" required oninput="updateCharacterCount(this)"></textarea>
+                <textarea wire:model="descripcion" class="qsrp-form-textarea" oninput="updateCharacterCount(this)"></textarea>
                 <div class="text-right">
                     <span id="charCount" class="text-gray-600">
                         <span id="remainingCount">500</span>/500 caracteres restantes
@@ -51,6 +49,7 @@
         </form>
     </div>
 </div>
+
 
 <script>
     function updateCharacterCount(textarea) {

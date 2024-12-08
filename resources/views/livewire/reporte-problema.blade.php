405ebcfc4e3ea3485b7fdd7443aad151a9049c5d
LAOV123 <laortez@unah.hn>
2024-12-07 19:47:19 -0600

    Agregado de required para reporte de problemas y para quejas y sugerencias

--- resources/views/livewire/reporte-problema.blade.php
+++ resources/views/livewire/reporte-problema.blade.php
@@ -9,7 +9,7 @@
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div class="qsrp-form-group">
                     <label class="qsrp-form-label">Título</label>
-                    <input type="text" wire:model="titulo" class="qsrp-form-input" maxlength="30" oninput="updateTitleCharacterCount(this)">
+                    <input type="text" wire:model="titulo" class="qsrp-form-input" required pattern="[a-zA-Z0-9\sáéíóúÁÉÍÓÚñÑ]+" title="El título solo debe contener letras, números y espacios." maxlength="30" oninput="updateTitleCharacterCount(this)">
                     <div class="text-right">
                         <span id="titleCharCount" class="text-gray-600">
                             <span id="titleRemainingCount">30</span>/30 caracteres restantes
@@ -19,7 +19,7 @@
                 </div>
                 <div class="qsrp-form-group">
                     <label class="qsrp-form-label">Sección</label>
-                    <select wire:model="seccion" class="qsrp-form-select">
+                    <select wire:model="seccion" class="qsrp-form-select" required title="Debe seleccionar un tipo válido: 'Queja' o 'Sugerencia'.">
                         <option value="">Seleccione una sección</option>
                         <option value="Inicio">Inicio</option>
                         <option value="Productos">Productos</option>
@@ -37,7 +37,7 @@
             </div>
             <div class="qsrp-form-group">
                 <label class="qsrp-form-label">Descripción</label>
-                <textarea wire:model="descripcion" class="qsrp-form-textarea" maxlength="500" oninput="updateCharacterCount(this)"></textarea>
+                <textarea wire:model="descripcion" class="qsrp-form-textarea" required  pattern="[a-zA-Z0-9\sáéíóúÁÉÍÓÚñÑ.,!?]+" title="La descripción solo debe contener letras, números y signos de puntuación básicos." maxlength="500" oninput="updateCharacterCount(this)"></textarea>
                 <div class="text-right">
                     <span id="charCount" class="text-gray-600">
                         <span id="remainingCount">500</span>/500 caracteres restantes
